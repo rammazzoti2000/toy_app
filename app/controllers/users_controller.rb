f3b67e131fea019d15988e9dc16eef71541c5956
Alexandru <rammazzoti2000@gmail.com>
2020-04-09 11:18:41 +0200

    edit show method to show first user  micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
+    @micropost = @user.microposts.first 
   end
 
   # GET /users/new
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,4 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, length: { maximum: 140 }, presence: true
-  
-
 end
